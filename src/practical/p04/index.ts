e7c4998fb9a1a48449e159a652f4e9f7aa7a42f3
Supawit Siripan <68011827@kmitl.ac.th>
2026-01-16 10:49:44 +0700

    68011827

--- src/practical/p04/index.ts
+++ src/practical/p04/index.ts
@@ -3,27 +3,27 @@ import axios from "axios"
 type ApiComment = {
   postId: number | null | undefined
 }
+
 export async function countCommentsByPost(): Promise<Record<number, number>> {
-  try{
+  try {
     const response = await axios.get<ApiComment[]>(
       "https://jsonplaceholder.typicode.com/comments"
     )
     const comments = response.data
 
-    if(comments.length === 0){
-      return{}
+    if (comments.length === 0) {
+      return {}
     }
 
-    return comments.reduce<Record<number,number>>((acc, comment) => {
+    return comments.reduce<Record<number, number>>((acc, comment) => {
       const postId = comment.postId
-      if(typeof postId !== "number"){
+      if (typeof postId !== "number") {
         return acc
       }
-      acc[postId] = (acc[postID] ?? 0) + 1
+      acc[postId] = (acc[postId] ?? 0) + 1
       return acc
     }, {})
-  }
-  catch {
-    return{}
+  } catch {
+    return {}
   }
 }
