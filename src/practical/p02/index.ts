782e6403ffc2330995e552b5431760564537013f
Supawit Siripan <68011827@kmitl.ac.th>
2026-01-16 09:57:48 +0700

    68011827

--- src/practical/p02/index.ts
+++ src/practical/p02/index.ts
@@ -1,1 +1,27 @@
-export function getPostsByUser() {}
+import axios from "axios"
+
+type ApiPost = {
+  userId: number
+  id: number
+  title: string
+}
+
+type Post = {
+  id: number
+  title: string
+}
+
+export async function getPostsByUser(userId: number): Promise<Post[]> {
+  try {
+    const response = await axios.get<ApiPost[]>(
+      "https://jsonplaceholder.typicode.com/posts"
+    )
+    const posts = response.data
+
+    return posts
+      .filter((post) => post.userId === userId)
+      .map(({ id, title }) => ({ id, title }))
+  } catch {
+    return []
+  }
+}
