a119ac4d84ce396c3c58cfb6cb88a1667442906d
Supawit Siripan <68011827@kmitl.ac.th>
2026-01-16 10:48:17 +0700

    68011827

--- src/practical/p04/index.ts
+++ src/practical/p04/index.ts
@@ -1,1 +1,29 @@
-export function countCommentsByPost() {}
+import axios from "axios"
+
+type ApiComment = {
+  postId: number | null | undefined
+}
+export async function countCommentsByPost(): Promise<Record<number, number>> {
+  try{
+    const response = await axios.get<ApiComment[]>(
+      "https://jsonplaceholder.typicode.com/comments"
+    )
+    const comments = response.data
+
+    if(comments.length === 0){
+      return{}
+    }
+
+    return comments.reduce<Record<number,number>>((acc, comment) => {
+      const postId = comment.postId
+      if(typeof postId !== "number"){
+        return acc
+      }
+      acc[postId] = (acc[postID] ?? 0) + 1
+      return acc
+    }, {})
+  }
+  catch {
+    return{}
+  }
+}
